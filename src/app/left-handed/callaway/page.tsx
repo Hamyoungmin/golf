cab1b181e2ad72cde8b4da8c6e6f1d0db8705e5f
kangto2003-boop <kangto2003@gmail.com>
2025-08-19 19:03:18 +0900

    Add left-handed callaway routing fix - copy main left-handed product and create dedicated product page

--- src/app/left-handed/callaway/page.tsx
+++ src/app/left-handed/callaway/page.tsx
@@ -1,18 +1,19 @@
 import ProductList from '@/components/ProductList';
 
 const callawayLeftHanded = [
-  { id: 1, name: 'PARADYM 10.5도 왼손용 Project X HZRDUS Smoke IM10 60', price: '가격문의', image: null },
-  { id: 2, name: 'ROGUE ST MAX 10.5도 왼손용 Fujikura VENTUS Blue 6 R', price: '가격문의', image: null },
-  { id: 3, name: 'EPIC MAX LS 9.5도 왼손용 Project X HZRDUS Smoke IM10', price: '가격문의', image: null },
-  { id: 4, name: 'EPIC SPEED 10.5도 왼손용 Diamana 50 for Callaway S', price: '가격문의', image: null },
-  { id: 5, name: 'MAVRIK LITE 12도 왼손용 Diamana 40 for Callaway L', price: '가격문의', image: null },
-  { id: 6, name: 'MAVRIK 10.5도 왼손용 4 SR', price: '가격문의', image: null },
-  { id: 7, name: 'ROGUE STAR 10.5도 왼손용 Speeder EVOLUTION R', price: '가격문의', image: null },
-  { id: 8, name: 'XR16 10.5도 왼손용 SR', price: '가격문의', image: null },
-  { id: 9, name: 'WHITE HOT VERSA TWELVE DB 왼손용 말렛', price: '가격문의', image: null },
-  { id: 10, name: 'JAWS RAW 56도 왼손용 NSPRO 950GH', price: '가격문의', image: null },
-  { id: 11, name: 'APEX DCB 7I-P 왼손용 NSPRO 950GH', price: '가격문의', image: null },
-  { id: 12, name: 'ROGUE ST MAX 15도 왼손용 Fujikura VENTUS Blue 6 R', price: '가격문의', image: null }
+  { id: 1, name: '캘러웨이 엘리트 10.5도', price: '600,000원', image: '/z1.jpg' },
+  { id: 2, name: 'PARADYM 10.5도 왼손용 Project X HZRDUS Smoke IM10 60', price: '가격문의', image: null },
+  { id: 3, name: 'ROGUE ST MAX 10.5도 왼손용 Fujikura VENTUS Blue 6 R', price: '가격문의', image: null },
+  { id: 4, name: 'EPIC MAX LS 9.5도 왼손용 Project X HZRDUS Smoke IM10', price: '가격문의', image: null },
+  { id: 5, name: 'EPIC SPEED 10.5도 왼손용 Diamana 50 for Callaway S', price: '가격문의', image: null },
+  { id: 6, name: 'MAVRIK LITE 12도 왼손용 Diamana 40 for Callaway L', price: '가격문의', image: null },
+  { id: 7, name: 'MAVRIK 10.5도 왼손용 4 SR', price: '가격문의', image: null },
+  { id: 8, name: 'ROGUE STAR 10.5도 왼손용 Speeder EVOLUTION R', price: '가격문의', image: null },
+  { id: 9, name: 'XR16 10.5도 왼손용 SR', price: '가격문의', image: null },
+  { id: 10, name: 'WHITE HOT VERSA TWELVE DB 왼손용 말렛', price: '가격문의', image: null },
+  { id: 11, name: 'JAWS RAW 56도 왼손용 NSPRO 950GH', price: '가격문의', image: null },
+  { id: 12, name: 'APEX DCB 7I-P 왼손용 NSPRO 950GH', price: '가격문의', image: null },
+  { id: 13, name: 'ROGUE ST MAX 15도 왼손용 Fujikura VENTUS Blue 6 R', price: '가격문의', image: null }
 ];
 
 export default function CallawayLeftHanded() {
