b2878f9a1a435a8bf61d02f2efb6c4921ca61d03
kangto2003-boop <kangto2003@gmail.com>
2025-08-19 13:20:47 +0900

    Remove admin setup text, keep icon only

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
                 d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" 
               />
             </svg>
-            관리자 설정
+
           </Link>
         </div>
       </div>
